fe928f03e38d302442147e7b051dcb4f8e341df2
zsadov1 <zsadov1@gmail.com>
2020-03-20 20:13:47 -0400

    Service worker ish finshed.

---
+++ public/db.js
@@ -0,0 +1,61 @@
+// check for indexedDB browser support
+
+let db = request.result;
+// create a new db request for a "budget" database.
+const request = window.indexedDB.open("budget", 1);
+request.onupgradeneeded = function(event) {
+  // create object store called "pending" and set autoIncrement to true
+  const budgetStore = db.createObjectStore("pending");
+
+};
+
+request.onsuccess = function(event) {
+  const db = target.result; 
+
+  if (navigator.onLine) {
+    checkDatabase();
+  }
+};
+
+request.onerror = function(event) {
+  // log error here
+};
+
+function saveRecord(record) {
+  // create a transaction on the pending db with readwrite access
+  const transaction = db.transaction(["pending"], "readwrite");
+
+  // access your pending object store
+  const pendingStore = transaction.objectStore("pending");
+
+  // add record to your store with add method.
+
+}
+
+function checkDatabase() {
+  // open a transaction on your pending db
+  // access your pending object store
+  // get all records from store and set to a variable
+
+  getAll.onsuccess = function() {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json"
+        }
+      })
+      .then(response => response.json())
+      .then(() => {
+          // if successful, open a transaction on your pending db
+          // access your pending object store
+          // clear all items in your store
+      });
+    }
+  };
+}
+
+// listen for app coming back online
+window.addEventListener("online", checkDatabase);
